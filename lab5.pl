315d2a7d775bd9a30af38c4a9869ffa999e9a1ba
jordanxlau <jordanxlau@yahoo.ca>
2024-10-12 10:38:28 -0400

    initial commit

---
+++ lab5.pl
@@ -0,0 +1,14 @@
+/* Parent-child relationships */
+parent(john, mary).
+parent(john, tom).
+parent(mary, ann).
+parent(mary, fred).
+parent(tom, liz).
+
+/* Genders*/
+male(john).
+male(tom).
+male(fred).
+female(mary).
+female(ann).
+female(liz).
