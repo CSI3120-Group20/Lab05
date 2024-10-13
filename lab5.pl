0a82d55acf1521957eb92f7193c9efa94f58c62a
jordanxlau <jordanxlau@yahoo.ca>
2024-10-13 16:03:42 -0400

    implemented task B

--- lab5.pl
+++ lab5.pl
@@ -12,3 +12,14 @@ male(fred).
 female(mary).
 female(ann).
 female(liz).
+
+% Task B
+% Auxiliary predicate used to test if N is odd or even
+odd(N) :- R is N mod 2, R == 1.
+
+% Base case
+sum_odd_numbers([],0).
+% Recursive case (even)
+sum_odd_numbers([H|T], Sum) :- not(odd(H)), sum_odd_numbers(T, Sum).
+% Recursive case (odd)
+sum_odd_numbers([H|T], Sum) :- odd(H), sum_odd_numbers(T, NewSum), Sum is NewSum + H.
